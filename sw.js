fb0cea54c272d401bfdbbefe9a2f9ce8abb74875
arlearning313-code <arlearning313@gmail.com>
2026-07-11 08:42:42 +0700

    Modul 0

---
+++ sw.js
@@ -0,0 +1,40 @@
+const NAMA_CACHE = "kaizen-v1"
+const FILE_INTI = ["./", "./index.html", "./manifest.json", "./kaizen-logo-icon-192.png", "./kaizen-logo-icon-512.png"]
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches
+        .open(NAMA_CACHE)
+        .then((cache) => {
+            return cache.addAll(FILE_INTI)
+        })
+    )
+})
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches
+        .match(event.request)
+        .then((response) => {
+            if (response !== undefined) {
+                return response
+            }
+            return fetch(event.request)
+        })
+    )
+})
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches
+        .keys()
+        .then((cache) => {
+            return Promise.all(cache.map((satuNama) => {
+                if (satuNama === NAMA_CACHE) {
+                    return null
+                }
+                return caches.delete(satuNama)
+            }))
+        })
+    )
+})
